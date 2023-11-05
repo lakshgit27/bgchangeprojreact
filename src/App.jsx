2d2a24481c5d4f281362ec6ccb7b8a66229d5380
Laksh <yadavlakshya86@gmail.com>
2023-11-05 10:32:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [color,setColor] = useState("olive")
+
+  return (
+    <>
+    
+      
+    </>
+  )
+}
+
+export default App
